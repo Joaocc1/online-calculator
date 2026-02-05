251f7ae9fce09c1c589f9bda5f4df25c995bb094
Joao Colaco <joaoc86@gmail.com>
2026-02-05 11:19:55 +0000

    update variables when clicking any key except the del

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const keyboard = document.querySelector(".keyboard");
 const mainDisplay = document.querySelector(".main-display");
+const secondaryDisplay = document.querySelector(".secondary-display");
 
 let numOne = 2;
 let numTwo = 2;
@@ -22,9 +23,12 @@ keyboard.addEventListener("click", (e) => {
     !e.target.matches(".clear") &&
     !e.target.matches(".del")
   ) {
+    numOne = e.target.textContent;
     mainDisplay.textContent = mainDisplay.textContent + e.target.textContent;
   } else if (e.target.matches(".clear")) {
+    numOne = numTwo = operator = "";
     mainDisplay.textContent = 0;
+    secondaryDisplay.textContent = "";
   } else if (e.target.matches(".del")) {
     mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
   }
