6e87fb16e8c727da779d169233c7a4a9a833b727
Joao Colaco <114249810+Joaocc1@users.noreply.github.com>
2026-02-07 16:26:59 +0000

    refactor event listeners and create logic to calculate math operations based on user input

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const keyboard = document.querySelector(".keyboard");
 const mainDisplay = document.querySelector(".main-display");
 const secondaryDisplay = document.querySelector(".secondary-display");
+const numberKeys = document.querySelectorAll(".num");
+const operatorKeys = document.querySelectorAll(".operator");
+const eraseKeys = document.querySelectorAll(".erase");
 
 let numOne = "";
 let numTwo = "";
 let operator = "";
+let result = "";
 
-let updateVar = "num-one";
+let updateVar = "num-one"; // to check which variable to update once a number is pressed
 
 const operators = {
   "+": (a, b) => a + b,
@@ -16,23 +20,76 @@ const operators = {
 };
 
 function operate(a, b, op) {
-  return operators[op](a, b);
+  // first turn a and b into typeof number
+  numA = Number(a);
+  numB = Number(b);
+  return operators[op](numA, numB);
 }
 
-keyboard.addEventListener("click", (e) => {
-  if (
-    e.target.matches(".key") &&
-    !e.target.matches(".clear") &&
-    !e.target.matches(".del")
-  ) {
-    numOne += e.target.textContent;
-    mainDisplay.textContent = numOne;
-  } else if (e.target.matches(".clear")) {
-    numOne = numTwo = operator = "";
-    mainDisplay.textContent = 0;
-    secondaryDisplay.textContent = "";
-  } else if (e.target.matches(".del")) {
-    numOne = numOne.slice(0, -1);
-    mainDisplay.textContent = numOne;
-  }
+numberKeys.forEach((key) => {
+  key.addEventListener("click", (e) => {
+    if (updateVar === "num-one") {
+      numOne += e.target.textContent;
+      mainDisplay.textContent = numOne;
+    } else if (updateVar === "num-two") {
+      numTwo += e.target.textContent;
+      mainDisplay.textContent = `${numOne}${operator}${numTwo}`;
+    }
+  });
 });
+
+operatorKeys.forEach((key) => {
+  key.addEventListener("click", (e) => {
+    console.log(updateVar);
+    if (e.target.textContent === "=" || updateVar === "num-two") {
+      result = operate(numOne, numTwo, operator);
+      numOne = result;
+      numTwo = "";
+      operator = e.target.textContent;
+      mainDisplay.textContent = result;
+    }
+    if (e.target.textContent !== "=" && updateVar === "num-one") {
+      operator = e.target.textContent;
+      updateVar = "num-two";
+      mainDisplay.textContent = `${numOne}${operator}`;
+    }
+  });
+});
+
+eraseKeys.forEach((key) => {
+  key.addEventListener("click", (e) => {
+    if (e.target.matches(".clear")) {
+      numOne = numTwo = operator = "";
+      mainDisplay.textContent = "";
+      secondaryDisplay.textContent = "";
+      updateVar = "num-one";
+    } else if (e.target.matches(".del")) {
+      numOne = numOne.slice(0, -1);
+      mainDisplay.textContent = numOne;
+    }
+  });
+});
+
+// keyboard.addEventListener("click", (e) => {
+//   if (
+//     e.target.matches(".key") &&
+//     !e.target.matches(".clear") &&
+//     !e.target.matches(".del")
+//   ) {
+//     numOne += e.target.textContent;
+//     mainDisplay.textContent = numOne;
+//   } else if (e.target.matches(".clear")) {
+//     numOne = numTwo = operator = "";
+//     mainDisplay.textContent = 0;
+//     secondaryDisplay.textContent = "";
+//   } else if (e.target.matches(".del")) {
+//     numOne = numOne.slice(0, -1);
+//     mainDisplay.textContent = numOne;
+//   }
+// });
+
+// to do:
+// Comment out the whole addEventListener above, create one for just the numbers (add a class of num in index.html)
+// Create the logic to update numOne and if an operator, a + for example is pressed, update the operator e move on to update the numTwo. If another operator or the = sign is press run operate()
+// Create an addEventListener for operators, when one is pressed update operator variable, change the updateVar to numTwo and display numOne + operator
+// add logic to check updateVar to know what variable to update, numOne or numTwo, and display that
